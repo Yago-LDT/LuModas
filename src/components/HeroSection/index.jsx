d4eb8286fd6b2f3971394865e206add84de6ac7e
Yago Pereira <yagopereiranunes6@gmail.com>
2025-11-21 17:54:30 -0300

    arrumando botão 3

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -6,7 +6,7 @@ function HeroSection(){
         <div className="heroSection fade">
             <h1>Bem vindo à <span>Lu Modas</span></h1>
             <h2>Serviços de costura, revenda de cosméticos e enxovais de qualidade em Águas Lindas de Goiás!</h2>
-            <button onclick="window.scrollTo({ top: 700, behavior: 'smooth' })">Conheça mais!</button>
+            <button onClick={() => {window.scrollTo({ top: 680, behavior: 'smooth' })}}>Conheça mais!</button>
         </div>
         </>
     )
